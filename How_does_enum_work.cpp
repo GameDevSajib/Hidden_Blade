daedacdf7c6e2a53e9cb0081f44901a6cc2733e9
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-10-09 04:09:26 +0600

    Add files via upload

---
+++ How_does_enum_work.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define do {
+#define done }
+#define read cin>>
+#define echo cout<<
+#define start_ int main()
+#define finish return 0
+
+using namespace std;
+
+enum friends{
+
+  pranto,
+  saad,
+  johan,
+  sabir,
+  joy,
+  sayem
+
+};
+start_
+do
+    for(ll i=pranto;i<sayem;i++)
+    do
+         echo i<<endl;
+    done
+
+
+
+
+finish;
+
+done
