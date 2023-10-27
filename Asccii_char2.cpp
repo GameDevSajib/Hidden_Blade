092d1d08317aced5765e0a1a44a5cf6392a97c96
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-10-28 04:02:14 +0600

    Add files via upload

---
+++ Asccii_char2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define do {
+#define done }
+#define finish return 0;
+#define start_ int main()
+using namespace std;
+start_
+do
+       for(ll i='A';i<='Z';i++)
+       do
+             cout<<(char)i<<endl;
+       done
+
+
+
+       finish;
+done
