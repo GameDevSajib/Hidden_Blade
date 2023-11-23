62bcbf96bcf9d93143ebecb05d773df0a39bf693
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-11-23 14:05:14 +0600

    Add files via upload

---
+++ Execute_file.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define do {
+#define done }
+#define finish return 0;
+#define start_ int main()
+
+void ExecuteBinaryFile()
+do
+       const char* executablePath = "/home/sajib/Hack_and_algo";
+       int result = system(executablePath);
+
+done
+
+using namespace std;
+start_
+do
+
+ExecuteBinaryFile();
+
+
+finish
+done
